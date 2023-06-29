537b0d5ca6279b5f36e91b6a1b92ddf7c017d696
Manuu <125753742+Manusm2712@users.noreply.github.com>
2023-06-29 17:27:15 -0500

    Add files via upload

---
+++ TrabajoArchivos/src/main/java/com/mycompany/trabajoarchivos/TrabajoArchivos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajoarchivos;
+
+/**
+ *
+ * @author maria
+ */
+public class TrabajoArchivos {
+
+    public static void main(String[] args) {
+        Interfaz miVentanaPrincipal = new Interfaz ();
+        miVentanaPrincipal.setVisible(true);
+
+    }
+}
